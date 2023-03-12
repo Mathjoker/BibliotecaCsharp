15513757d1f37ad1f2e46660a8d25658cafcf924
Matheus Bitencourt <matheus_si_kta@outlook.com>
2023-03-12 11:32:12 -0300

    Classe funcionario criada.

---
+++ Biblioteca/Models/Funcionarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models {
+    internal class Funcionarios :Pessoa{
+
+        public int Matricula { get; set; }
+        public String Cargo { get; set; }
+
+
+        public Funcionarios() {
+
+        }
+
+        public Funcionarios(string nome, DateTime dataNascimento, string cPF, string email, string senha, string telefone, int matricula, String cargo) 
+            : base(nome, dataNascimento, cPF, email, senha, telefone){
+            Matricula = matricula;
+            Cargo = cargo;
+        }
+
+
+    }
+}
